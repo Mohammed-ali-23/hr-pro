39ad61ef1ae1237e582d0984e9e787a65bb88d3c
Mohammed AL-Manee <mohammedal-manee@MacBook-Pro-Mohammed.local>
2023-01-11 20:38:11 +0300

    done

--- hr_pro.py
+++ hr_pro.py
@@ -1,7 +1,47 @@
+
 class Employee:
+    def __init__(self,name, age, salary, employemnt_years):
+      self.name = name
+      self.age = age
+      self.salary = salary
+      self.employemnt_years = employemnt_years
+    
+    def get_annual_salary(self):
+        return self.salary * 12
+
+
+
+
+
+
+
+
    #Employee class here
 
 class Manager(Employee):
+    def __init__(self,name, age, salary, employment_years, bonus_percentage):
+      self.name = name
+      self.age = age 
+      self.salary = salary
+      self.employment_years = employment_years
+      self.bonus_percentage = bonus_percentage
+
+    def bonus_percentage(self):
+
+         return(self.bonus_percentage * self.salary)
+    def __str__(self.manager) -> str:
+        return super().__str__()
+        
+        
+       Employee = []
+
+         
+
+        
+
+
+
+
     #Manager class here
         
 def main():
